086ff94fe1d087a5072b9fa53a0e817991676d71
annamatveeva-programmer <amatveeva.ieu2023@student.ie.edu>
2025-02-27 19:06:01 +0100

    Add files via upload

---
+++ S3-4.py
@@ -0,0 +1,19 @@
+print(2+6)
+print(9-99)
+print(3*8)
+print(22/5) #Produces float result
+print(22//5) #Integer division
+print(22%5) #"resto" of the division
+print(2**3) #Exponential
+print(not True)
+print(not False)
+print(not 7) #False because 7 is True
+print(True or False)
+print(True and False)
+print(2 or 3 or 4)
+print( False or 0 or 4 or True)
+print(1 and 2 and 3)
+print(1 and 0.0 and 4)
+print(False or 0 or None)
+print("Anna")
+
